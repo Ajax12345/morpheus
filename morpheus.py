a00ef68e9124dd051a1c1cdf465065dbe4347dd2
James Petullo <pentester18@gmail.com>
2018-10-11 19:10:56 -0400

    Update morpheus.py

--- morpheus.py
+++ morpheus.py
@@ -1,5 +1,12 @@
 import socket, pickle
-import jnet_port_forwarding
+import interal_jnet_port_forwarding as jnet_port_forwarding
+import typing
+
+def on_connection(_func:typing.Callable) -> typing.Callable:
+    def _wrapper(_cls):
+        setattr(_cls, 'on_accept_connection', _func)
+        return _cls
+    return _wrapper
 
 class Server:
     @jnet_port_forwarding.greet()
@@ -10,17 +17,20 @@ class Server:
         self._socket.bind((self.main_host, self.port))
         self._socket.listen(self._max_connections)
         return self
-    def accept_connections(self, on_recieve=lambda _mes:{'echo':_mes}, _rec_size = 1024, display_host=True):
+    def accept_connections(self):
         while True:
             try:
                 c, addr = self._socket.accept()
             except:
                 pass
             else:
+                '''
                 print(f'Recieved connnection from {addr[0]}')
                 _data = c.recv(_rec_size)
                 print(f'Got {_data} from {addr[0]}')
                 c.sendall(pickle.dumps(on_recieve(pickle.loads(_data))))
+                '''
+                self.__class__.on_accept_connection(c, addr)
     @jnet_port_forwarding.PortManager.reset_mapping
     def __exit__(self, *args):
         print(self.__dict__)
@@ -48,16 +58,22 @@ class Client:
 
 if __name__ == '__main__':
 
+    def recieve_connection(_client, addr):
+        print('this is new here')
+        print(f'Recieved connnection from {addr[0]}')
+        _data = _client.recv(1024)
+        print(f'Got {_data} from {addr[0]}')
+        _client.sendall(pickle.dumps({'echo':pickle.loads(_data)}))
+    
+    @on_connection(recieve_connection)
     class Manage_Server(Server):
-
         _max_connections = 5
         main_host = ''
         lan_ip = jnet_port_forwarding.PortManager.get_my_ip()
-        root = 'PASSWORDSTUFF'
+        root = 'zorro16'
         port = 6000
         from_port = 8423
         forward_ports = True
 
     with Manage_Server() as server:
         server.accept_connections()
-    
